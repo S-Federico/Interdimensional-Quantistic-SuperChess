b2165706468672f2c7c6e6e8566b2a0e01f19622
LoZimmero <fiorettigabriele@gmail.com>
2024-09-16 21:18:43 +0200

    WIP load game

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,16 +8,26 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public GameObject continueButton;
+    public GameObject LoadGamePanel;
+    public GameObject MainMenuPanel;
     void Start(){
         GameManager gm = GameManager.Instance;
         if(gm.IsSaveFilePresent()){
             continueButton.SetActive(true);
         }
+        Reset();
     }
     public void NewGame() {
         GameManager.Instance.NewGame();
     }
     public void ContinueGame(){
-        GameManager.Instance.ContinueGame();
+        //GameManager.Instance.ContinueGame();
+        LoadGamePanel.SetActive(true);
+        MainMenuPanel.SetActive(false);
+    }
+
+    public void Reset() {
+        LoadGamePanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
     }
 }
