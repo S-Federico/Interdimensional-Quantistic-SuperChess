c140b8775901a32f35b57856bee5511af6238338
LoZimmero <fiorettigabriele@gmail.com>
2024-11-10 18:53:35 +0100

    Small fix

--- Assets/Scripts/Architecture/AssetsManager.cs
+++ Assets/Scripts/Architecture/AssetsManager.cs
@@ -18,7 +18,7 @@ public class AssetsManager : MonoBehaviour
     private GameManager gameManager;
     private AssetsManager() {}
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Instance = this;
         DontDestroyOnLoad(this);
